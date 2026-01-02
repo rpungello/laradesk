a9278933d8a5439f2464ea7ad2a19ab199d910ef
Rob Pungello <rob@rpun.io>
2026-01-02 09:28:03 -0500

    Configure app defaults

--- config/app.php
+++ config/app.php
@@ -13,7 +13,20 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Laradesk'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Version
+    |--------------------------------------------------------------------------
+    |
+    | This value is the version of your application. This value is used when
+    | the framework needs to place the application's version in a notification
+    | or any other location as required by the application or its packages.
+    |
+    */
+
+    'version' => env('APP_VERSION', 'dev-master'),
 
     /*
     |--------------------------------------------------------------------------
