b03b15acee7281a0d0480fc5f6f47001d4d1f987
rpungello <3287663+rpungello@users.noreply.github.com>
2026-01-02 16:04:25 +0000

    Fix styling

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ use App\Enums\UserRole;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
