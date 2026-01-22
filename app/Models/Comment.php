1b8df63321dc4bf70f989d8baf08155ea8b5220d
rpungello <3287663+rpungello@users.noreply.github.com>
2026-01-22 20:04:00 +0000

    Fix styling

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -11,7 +11,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Support\Facades\Storage;
 use OwenIt\Auditing\Contracts\Auditable;
 
 #[ObservedBy([CommentObserver::class])]
